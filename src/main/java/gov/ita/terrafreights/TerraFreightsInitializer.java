c056386c9e9063f8d4e1e044ca2eccf5958ccd89
lipsotiko <lipsotiko@gmail.com>
2019-06-18 17:03:42 -0400

    Added logic to only seed the database when there is no tariff data loaded

--- src/main/java/gov/ita/terrafreights/TerraFreightsInitializer.java
+++ src/main/java/gov/ita/terrafreights/TerraFreightsInitializer.java
@@ -1,5 +1,6 @@
 package gov.ita.terrafreights;
 
+import gov.ita.terrafreights.tariff.TariffRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -8,13 +9,16 @@ import org.springframework.stereotype.Component;
 public class TerraFreightsInitializer implements ApplicationListener<ContextRefreshedEvent> {
 
   private DataSeeder dataSeeder;
+  private TariffRepository tariffRepository;
 
-  public TerraFreightsInitializer(DataSeeder dataSeeder) {
+  public TerraFreightsInitializer(DataSeeder dataSeeder, TariffRepository tariffRepository) {
     this.dataSeeder = dataSeeder;
+    this.tariffRepository = tariffRepository;
   }
 
   @Override
   public void onApplicationEvent(ContextRefreshedEvent event) {
-    dataSeeder.seed();
+    if(tariffRepository.count() == 0)
+      dataSeeder.seed();
   }
 }
---
+++ src/test/java/gov/ita/terrafreights/TerraFreightsInitializerTest.java
@@ -0,0 +1,39 @@
+package gov.ita.terrafreights;
+
+import gov.ita.terrafreights.tariff.TariffRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TerraFreightsInitializerTest {
+
+  @Mock
+  private DataSeeder dataSeeder;
+
+  @Mock
+  private TariffRepository tariffRepository;
+
+  @Mock
+  private ContextRefreshedEvent contextRefreshedEvent;
+
+  @Test
+  public void database_is_seeded_when_no_tariff_data_exists() {
+    TerraFreightsInitializer terraFreightsInitializer = new TerraFreightsInitializer(dataSeeder, tariffRepository);
+    terraFreightsInitializer.onApplicationEvent(contextRefreshedEvent);
+    verify(dataSeeder, times(1)).seed();
+  }
+
+  @Test
+  public void database_is_NOT_seeded_when_tariff_data_exists() {
+    when(tariffRepository.count()).thenReturn(100L);
+    TerraFreightsInitializer terraFreightsInitializer = new TerraFreightsInitializer(dataSeeder, tariffRepository);
+    terraFreightsInitializer.onApplicationEvent(contextRefreshedEvent);
+    verify(dataSeeder, times(0)).seed();
+  }
+
+}
