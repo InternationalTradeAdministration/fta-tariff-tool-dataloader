0bc5ac5e1a7574a42aaec4127c1399f7fd8c3de2
lipsotiko <lipsotiko@gmail.com>
2019-07-19 16:12:42 -0400

    Pulling the username (email) out of the security context for auditing uploads

--- src/main/java/gov/ita/terrafreights/security/DevelopmentAuthenticationFacade.java
+++ src/main/java/gov/ita/terrafreights/security/DevelopmentAuthenticationFacade.java
@@ -6,8 +6,10 @@ import org.springframework.stereotype.Service;
 @Profile({"development", "staging"})
 @Service
 public class DevelopmentAuthenticationFacade implements AuthenticationFacade {
+
   @Override
   public String getUserName() {
-    return "Test User";
+    return "TestUser@trade.gov";
   }
+
 }
--- src/main/java/gov/ita/terrafreights/security/ProductionAuthenticationFacade.java
+++ src/main/java/gov/ita/terrafreights/security/ProductionAuthenticationFacade.java
@@ -2,14 +2,18 @@ package gov.ita.terrafreights.security;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.stereotype.Service;
 
 @Service
 @Profile("production")
 public class ProductionAuthenticationFacade implements AuthenticationFacade {
+
   @Override
   public String getUserName() {
-    return SecurityContextHolder.getContext().getAuthentication().getName();
+    return ((DefaultOidcUser) SecurityContextHolder.getContext().getAuthentication()
+      .getPrincipal()).getAttributes().get("upn").toString();
   }
+
 }
 
